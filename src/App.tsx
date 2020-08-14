46f90c09354e8f46fa56b8e0e6d205510ddc4f59
Suyash458 <suyash.behera458@gmail.com>
2020-08-14 12:25:28 +0530

    add file dropzone and basic css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback} from 'react';
 import './App.css';
+import {useDropzone } from 'react-dropzone';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 function App() {
+
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={(file as any).path}>
+      {(file as any).path} - {file.size} bytes
+    </li>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid fluid>
+      <Row>
+        <Col xs={4}></Col>
+        <Col xs={4}>
+          <h2>autoindex</h2>
+          <p>
+            automatically add a navigable index to your PDFs
+          </p>
+          <div className="Dropzone">
+              <section className="container">
+                <div {...getRootProps({className: 'Dragzone'})}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+                <aside>
+                  <h4>Files</h4>
+                  <ul>{files}</ul>
+                </aside>
+              </section>
+          </div>
+        </Col>
+        <Col xs={4}></Col>         
+      </Row>        
+    </Grid>
   );
 }
 
